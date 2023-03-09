08b0d10a5b7278b5c5477e9d2134f41e5f2ce237
a2ron-k <aaronkwah@gmail.com>
2023-03-09 17:32:31 +0800

    feat: add scss styling guide

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.css";
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
+import "./assets/style/global.scss";
 
 const app = createApp(App)
 
