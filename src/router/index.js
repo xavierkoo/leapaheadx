b0c3ec27b916688d6f01dd4d2935883921060d07
Ray Quek <ray.quek@gmail.com>
2023-03-14 15:45:21 +0800

    feat: add form view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FormView from '../views/FormView.vue'
+import Try from '../views/TRy.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/formRender',
+      name: 'formRender',
+      component: FormView
+    },
+    {
+      path: '/try',
+      name: 'try',
+      component: Try
     },
     {
       path: '/about',
