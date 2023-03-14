7fea1d58293d26604c95f3a590cbfe898452df0a
a2ron-k <aaronkwah@gmail.com>
2023-03-14 21:08:00 +0800

    chore: remove redundant boilerplate code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -11,15 +10,12 @@ import HelloWorld from './components/HelloWorld.vue'
       href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
       rel="stylesheet"
     />
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/workflow">workflow</RouterLink>
+        <RouterLink to="/adminDashboard">Admin Dashboard</RouterLink>
       </nav>
     </div>
   </header>
