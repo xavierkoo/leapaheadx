b2c63a31f7a87c4cff91f851c395f69600b782f8
a2ron-k <aaronkwah@gmail.com>
2023-03-14 21:08:48 +0800

    fix: update router for admin dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import FormView from '../views/FormView.vue'
-import Try from '../views/TRy.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,21 +16,8 @@ const router = createRouter({
       component: FormView
     },
     {
-      path: '/try',
-      name: 'try',
-      component: Try
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/workflow',
-      name: 'workflow',
+      path: '/adminDashboard',
+      name: 'adminDashboard',
       component: () => import('../views/DashboardView.vue')
     },
   ]
